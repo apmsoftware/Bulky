ff3cb4b687ec60686657411d2a4fa7dc53462f19
apmsoftware <anishprashantmisal@gmail.com>
2023-07-18 15:06:32 +0530

    Display Category list to view

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+          _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList= _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
